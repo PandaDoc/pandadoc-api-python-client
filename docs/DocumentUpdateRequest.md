69731f9f4db81397b2eaaa68bbc094a9c4887f3e
akutsanov <93129045+akutsanov@users.noreply.github.com>
2022-08-18 18:37:34 +0300

    v5.3.0 (#14)
    - "Sender" parameter is added to the document send endpoint
    - Add a new endpoint "document update"

---
+++ docs/DocumentUpdateRequest.md
@@ -0,0 +1,16 @@
+# DocumentUpdateRequest
+
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**recipients** | [**[DocumentUpdateRequestRecipients]**](DocumentUpdateRequestRecipients.md) | The list of recipients you&#39;re sending the document to. The ID or email are required. If the ID is passed, an existing recipient will be updated. If the email is passed, a new recipient will be added to CC. | [optional] 
+**fields** | **{str: (bool, date, datetime, dict, float, int, list, str, none_type)}** | You may pass a list of fields/values which exist in a document. Please use &#x60;Merge Field&#x60; property of the fields like the key. | [optional] 
+**tokens** | [**[DocumentCreateByTemplateRequestTokens]**](DocumentCreateByTemplateRequestTokens.md) | You can pass a list of tokens/values. If a token name exists in a document then the value will be updated. Otherwise, a new token will be added to the document. | [optional] 
+**metadata** | **{str: (bool, date, datetime, dict, float, int, list, str, none_type)}** | You can pass arbitrary data in the key-value format to associate custom information with a document. This information is returned in any API requests for the document details by id. If metadata exists in a document then the value will be updated. Otherwise, metadata will be added to the document. | [optional] 
+**pricing_tables** | [**[PricingTableRequest]**](PricingTableRequest.md) |  | [optional] 
+**any string name** | **bool, date, datetime, dict, float, int, list, str, none_type** | any string name can be used but the value must be the correct type | [optional]
+
+[[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- pandadoc_client/api/documents_api.py
+++ pandadoc_client/api/documents_api.py
@@ -34,6 +34,7 @@ from pandadoc_client.model.document_status_request_enum import DocumentStatusReq
 from pandadoc_client.model.document_status_response import DocumentStatusResponse
 from pandadoc_client.model.document_transfer_all_ownership_request import DocumentTransferAllOwnershipRequest
 from pandadoc_client.model.document_transfer_ownership_request import DocumentTransferOwnershipRequest
+from pandadoc_client.model.document_update_request import DocumentUpdateRequest
 from pandadoc_client.model.linked_object_create_request import LinkedObjectCreateRequest
 from pandadoc_client.model.linked_object_create_response import LinkedObjectCreateResponse
 from pandadoc_client.model.linked_object_list_response import LinkedObjectListResponse
@@ -1002,6 +1003,65 @@ class DocumentsApi(object):
                 'location_map': {
                     'id': 'path',
                     'document_transfer_ownership_request': 'body',
+                },
+                'collection_format_map': {
+                }
+            },
+            headers_map={
+                'accept': [
+                    'application/json'
+                ],
+                'content_type': [
+                    'application/json'
+                ]
+            },
+            api_client=api_client
+        )
+        self.update_document_endpoint = _Endpoint(
+            settings={
+                'response_type': None,
+                'auth': [
+                    'apiKey',
+                    'oauth2'
+                ],
+                'endpoint_path': '/public/v1/documents/{id}',
+                'operation_id': 'update_document',
+                'http_method': 'PATCH',
+                'servers': None,
+            },
+            params_map={
+                'all': [
+                    'id',
+                    'document_update_request',
+                ],
+                'required': [
+                    'id',
+                    'document_update_request',
+                ],
+                'nullable': [
+                ],
+                'enum': [
+                ],
+                'validation': [
+                ]
+            },
+            root_map={
+                'validations': {
+                },
+                'allowed_values': {
+                },
+                'openapi_types': {
+                    'id':
+                        (str,),
+                    'document_update_request':
+                        (DocumentUpdateRequest,),
+                },
+                'attribute_map': {
+                    'id': 'id',
+                },
+                'location_map': {
+                    'id': 'path',
+                    'document_update_request': 'body',
                 },
                 'collection_format_map': {
                 }
@@ -2219,4 +2279,85 @@ class DocumentsApi(object):
         kwargs['document_transfer_ownership_request'] = \
             document_transfer_ownership_request
         return self.transfer_document_ownership_endpoint.call_with_http_info(**kwargs)
+
+    def update_document(
+        self,
+        id,
+        document_update_request,
+        **kwargs
+    ):
+        """Update Document only in the draft status  # noqa: E501
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.update_document(id, document_update_request, async_req=True)
+        >>> result = thread.get()
+
+        Args:
+            id (str): Document ID
+            document_update_request (DocumentUpdateRequest):
+
+        Keyword Args:
+            _return_http_data_only (bool): response data without head status
+                code and headers. Default is True.
+            _preload_content (bool): if False, the urllib3.HTTPResponse object
+                will be returned without reading/decoding response data.
+                Default is True.
+            _request_timeout (int/float/tuple): timeout setting for this request. If
+                one number provided, it will be total request timeout. It can also
+                be a pair (tuple) of (connection, read) timeouts.
+                Default is None.
+            _check_input_type (bool): specifies if type checking
+                should be done one the data sent to the server.
+                Default is True.
+            _check_return_type (bool): specifies if type checking
+                should be done one the data received from the server.
+                Default is True.
+            _spec_property_naming (bool): True if the variable names in the input data
+                are serialized names, as specified in the OpenAPI document.
+                False if the variable names in the input data
+                are pythonic names, e.g. snake case (default)
+            _content_type (str/None): force body content-type.
+                Default is None and content-type will be predicted by allowed
+                content-types and body.
+            _host_index (int/None): specifies the index of the server
+                that we want to use.
+                Default is read from the configuration.
+            async_req (bool): execute request asynchronously
+
+        Returns:
+            None
+                If the method is called asynchronously, returns the request
+                thread.
+        """
+        kwargs['async_req'] = kwargs.get(
+            'async_req', False
+        )
+        kwargs['_return_http_data_only'] = kwargs.get(
+            '_return_http_data_only', True
+        )
+        kwargs['_preload_content'] = kwargs.get(
+            '_preload_content', True
+        )
+        kwargs['_request_timeout'] = kwargs.get(
+            '_request_timeout', None
+        )
+        kwargs['_check_input_type'] = kwargs.get(
+            '_check_input_type', True
+        )
+        kwargs['_check_return_type'] = kwargs.get(
+            '_check_return_type', True
+        )
+        kwargs['_spec_property_naming'] = kwargs.get(
+            '_spec_property_naming', False
+        )
+        kwargs['_content_type'] = kwargs.get(
+            '_content_type')
+        kwargs['_host_index'] = kwargs.get('_host_index')
+        kwargs['id'] = \
+            id
+        kwargs['document_update_request'] = \
+            document_update_request
+        return self.update_document_endpoint.call_with_http_info(**kwargs)
 
